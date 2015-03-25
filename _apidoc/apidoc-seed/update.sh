c992eed14b42e45958168a3914db526a2e15b57a
Laurent Prevost <laurent.prevost@lotaris.com>
2015-03-25 13:42:17 +0100

    Added documentation for the modifications done in dockerization process and removed the hardcoded base URL. Use grunt prod --baseUrl=/api to set the base URL

--- _apidoc/apidoc-seed/update.sh
+++ _apidoc/apidoc-seed/update.sh
@@ -4,7 +4,7 @@ echo "Deleting previous version in ../../api/"
 rm -fr ../../api/*
 
 echo "Building new API site with grunt prod"
-grunt prod
+grunt prod --baseUrl=/api
 
 echo "Moving produced site in ../../api/"
 mv build/* ../../api/
