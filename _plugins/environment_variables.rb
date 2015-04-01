7220b81bc3515a40c44db445fc858166076d0144
Laurent Prevost <laurent.prevost@lotaris.com>
2015-04-01 17:23:49 +0200

    Fixed issue with environment variables

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -6,6 +6,7 @@ module Jekyll
 
 		def generate(site)
 			site.config['ifluxUrl'] = ENV['IFLUX_SERVER_URL'] || 'https://iflux.herokuapp.com'
+			site.config['apiUrl'] = ENV['IFLUX_API_URL'] || 'https://iflux.herokuapp.com'
 			# Add other environment variables to `site.config` here...
 		end
 
